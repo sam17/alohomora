d8c65f873bd67ee4264055bb976c235873c8dd5d
soumyadeep.m <soumyadeep.m@udaan.com>
2020-09-20 02:06:03 +0530

    First commit

---
+++ nodemcu_server/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+void setup()
+{
+  Serial.begin(9600);
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("Azkaban", "voldemort");
+  Serial.println("Connected!");
+}
+
+void loop()
+{
+  Serial.println("Idle...");
+  delay(1000);
+}
