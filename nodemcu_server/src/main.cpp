63c12e19bd6b5b4bdf9b287ef48c222eec4b7fe2
soumyadeep.m <soumyadeep.m@udaan.com>
2020-09-21 00:17:25 +0530

    Working code on nodemcu

--- nodemcu_server/src/main.cpp
+++ nodemcu_server/src/main.cpp
@@ -1,21 +1,61 @@
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
-
-#include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>
+#include <ESP8266mDNS.h>
+#include <DNSServer.h>
 
-void setup()
-{
-  Serial.begin(9600);
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("Azkaban", "voldemort");
-  Serial.println("Connected!");
+const char* wifiName = "Azkaban";
+const char* wifiPass = "voldemort";
+  
+ESP8266WebServer server(80);
+ 
+//Handles http request 
+void handleRoot() {
+  digitalWrite(16, 0);   //Blinks on board led on page request 
+  digitalWrite(5, 0);   //Blinks on board led on page request 
+
+  server.send(200, "text/plain", "hello from esp8266!");
 }
-
-void loop()
-{
-  Serial.println("Idle...");
-  delay(1000);
+ 
+ 
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+  Serial.begin(9600);
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  
+  Serial.print("Connecting to ");
+  Serial.println(wifiName);
+ 
+  WiFi.begin(wifiName, wifiPass);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
+ 
+   if(WiFi.status() == WL_CONNECTED) //If WiFi connected to hot spot then start mDNS
+  {
+    if (MDNS.begin("firang")) {  //Start mDNS with name esp8266
+      Serial.println("MDNS started");
+    }
+  }
+ 
+  server.on("/", handleRoot);  //Associate handler function to path
+    
+  server.begin();                           //Start server
+  Serial.println("HTTP server started");
+}
+ 
+// the loop function runs over and over again forever
+void loop() {
+  server.handleClient();
 }
