0ab66179d4b18332a9aa48e202fa787283c7e969
dementor <1422630+sam17@users.noreply.github.com>
2022-01-09 19:44:53 +0530

    Refactor to split into files

--- nodemcu_server/src/main.cpp
+++ nodemcu_server/src/main.cpp
@@ -1,140 +1,127 @@
-#include <Arduino.h>
-
+// #include <DNSServer.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <DNSServer.h>
-#include <ArduinoOTA.h>
+#include "battery_monitor.cpp"
+#include "OTA.cpp"
 
-const char* wifiName = "Azkaban";
-const char* wifiPass = "voldemort";
+#define BAUD_RATE 9600
+
+bool debug = false;
 
 ESP8266WebServer server(80);
- 
-//Handles http request 
-void handleRoot() {
-  digitalWrite(16, 0);   //Blinks on board led on page request 
-  digitalWrite(5, 0);   //Blinks on board led on page request 
 
+void handleRoot()
+{
+  digitalWrite(17, 0); //Blinks on board led on page request
+  digitalWrite(5, 0);  //Blinks on board led on page request
   server.send(200, "text/plain", "Hello from Alohomora!");
 }
 
-void resetPins() {
+void initDoorPins()
+{
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+}
+
+void resetPins()
+{
   digitalWrite(D0, LOW);
   digitalWrite(D1, LOW);
 }
 
-void closeDoor() {
-
+void closeDoor()
+{
   digitalWrite(D0, LOW);
   digitalWrite(D1, HIGH);
-  delay(2000);
-  resetPins();
-
+  // resetPins();
   server.send(200, "text/plain", "Close door.");
 }
- 
-void openDoor() {
 
+void openDoor()
+{
   digitalWrite(D0, HIGH);
   digitalWrite(D1, LOW);
-  delay(10000);
-  closeDoor();
-
+  // closeDoor();
   server.send(200, "text/plain", "Open door.");
 }
 
-void reset() {
+void reset()
+{
   resetPins();
   server.send(200, "text/plaIn", "Reset Done");
-  
 }
 
-void StartOTA() {
-
-  ArduinoOTA.setHostname("alohomora");
- ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-  Serial.println("Ready OTA");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+void door_loop() {
+  if(debug) {
+    Serial.println("Status DO D1:");
+    Serial.println(digitalRead(D0));
+    Serial.println(digitalRead(D1));
+  }
 }
 
+void app_loop()
+{
+  server.handleClient();
+}
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  
-  Serial.begin(9600);
-
-  Serial.println("Setting D0 and D1");
-  pinMode(D0, OUTPUT);
-  pinMode(D1, OUTPUT);
-  closeDoor();
+void app_server_init()
+{
+  const char *wifiName = "Azkaban";
+  const char *wifiPass = "voldemort";
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
-  
   Serial.print("Connecting to ");
   Serial.println(wifiName);
- 
   WiFi.begin(wifiName, wifiPass);
- 
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
- 
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
- 
-   if(WiFi.status() == WL_CONNECTED) //If WiFi connected to hot spot then start mDNS
+  Serial.println(WiFi.localIP());    //You can get IP address assigned to ESP
+  if (WiFi.status() == WL_CONNECTED) //If WiFi connected to hot spot then start mDNS
   {
     Serial.println(WiFi.status());
-    // if (MDNS.begin("firang")) {  //Start mDNS with name esp8266
-    //   Serial.println("MDNS started");
-    // }
+    if (MDNS.begin("alohomora"))
+    { //Start mDNS with name esp8266
+      Serial.println("MDNS started");
+    }
+
+    server.on("/", handleRoot);   //Associate handler function to path
+    server.on("/open", openDoor); //Associate handler function to path
+    server.on("/close", closeDoor);
+    server.on("/reset", reset); //Associate handler function to path
+    server.begin();             //Start server
+    Serial.println("HTTP server started");
   }
- 
-  StartOTA();
-  server.on("/", handleRoot);  //Associate handler function to path
-    
-  server.on("/open", openDoor);  //Associate handler function to path
-  server.on("/close", closeDoor);
-  server.on("/reset", reset);  //Associate handler function to path
-  
-  server.begin();                           //Start server
-  Serial.println("HTTP server started");
 }
 
+BatteryMonitor batteryMonitor = BatteryMonitor(A0, debug);
+OverTheAir overTheAir;
+// the setup function runs once when you press reset or power the board
+void setup()
+{
 
- 
+  Serial.begin(BAUD_RATE);
+  app_server_init();
+  batteryMonitor.setup();
+  overTheAir.startOTA();
+  delay(10);
+}
+
 // the loop function runs over and over again forever
-void loop() {
-   ArduinoOTA.handle();
-  server.handleClient();
+void loop()
+{
+  batteryMonitor.loop();
+  overTheAir.ota_loop();
+  app_loop();
+  door_loop();
+  delay(50);
 }
